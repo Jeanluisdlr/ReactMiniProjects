cf3b195ff06fd3e9c4491a47289394be57fb7871
Jean de la Rosa <jeanluisdelarosa97@gmail.com>
2023-11-14 04:14:34 -0400

    adding structure and initialize style

--- clicks-counter/src/App.js
+++ clicks-counter/src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
 import "./App.css";
+import freeCodeCampLogo from "./images/freeCodeCampLogo.svg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="principal-container">
+        <img
+          id="freeCodeCamp-logo"
+          src={freeCodeCampLogo}
+          alt="freeCodeCamp Logo"
+        />
+        <div className="display-counter">0</div>
+        <div className="buttons">
+          <button id="click">Click</button>
+          <button id="reset">Reset</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
