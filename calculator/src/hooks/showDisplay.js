105a4e9e5b24506d7ad10f293471c0e05d3f3247
Jean de la Rosa <jeanluisdelarosa97@gmail.com>
2024-01-17 21:57:09 -0400

    update

--- calculator/src/hooks/showDisplay.js
+++ calculator/src/hooks/showDisplay.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { useState } from "react";
-import { evaluate } from "mathjs";
+import { evaluate, exp } from "mathjs";
 
 const UseShowDisplay = () => {
   const [expression, setInput] = useState("");
-
-  const printButton = (valor) => setInput(expression + valor);
+  const sings = [".", "/", "*", "-", "+"];
+  const printButton = (valor) => {
+    expression === "" && !isNaN(valor)
+      ? setInput(expression + valor)
+      : expression === "" && sings.includes(valor)
+      ? setInput(0 + valor)
+      : !isNaN(expression.slice(-1)) && !isNaN(valor)
+      ? setInput(expression + valor)
+      : !isNaN(expression.slice(-1)) && sings.includes(valor)
+      ? setInput(expression + valor)
+      : sings.includes(expression.slice(-1)) && expression.slice(-1) === valor
+      ? setInput(expression)
+      : sings.includes(expression.slice(-1)) && !isNaN(valor)
+      ? setInput(expression + valor)
+      : setInput(expression.slice(0, -1) + valor);
+  };
   const clearDisplay = () => setInput("");
-  const evaluation = () => setInput(evaluate(expression));
+  const evaluation = () => {
+    expression === ""
+      ? setInput(expression)
+      : sings.includes(expression.slice(-1))
+      ? setInput(evaluate(expression + expression.slice(-1) + expression))
+      : setInput(evaluate(expression));
+  };
 
   return { expression, printButton, clearDisplay, evaluation };
 };
