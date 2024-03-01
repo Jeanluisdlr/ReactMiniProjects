5b9dd4b0724cedbcff52d6ed611a0ccc8b371b45
Jean de la Rosa <jeanluisdelarosa97@gmail.com>
2024-03-01 16:40:07 -0400

    update inserting text

--- exercises/src/components/InsertingText.js
+++ exercises/src/components/InsertingText.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../stylesheets/InsertingText.css";
 
 function InsertingText() {
+  const [active, setActive] = useState(false);
+  const [input, setinput] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleInput = (e) => {
+    setinput(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      setActive(!active);
+      setList([...list, input]);
+    }
+  };
+
   return (
-    <div className="insertingText-conatiner">
+    <div className="insertingText-container">
       <textarea
         id="text-area"
         placeholder="Enter your text here..."
-        re
+        value={input}
+        onChange={handleInput}
+        onKeyDown={handleKeyPress}
       ></textarea>
       <div className="document-container">
         <h6>Document</h6>
-        <div className="document-body"></div>
+        {active ? (
+          <div className="document-body">
+            <ul>
+              {list.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div
+            className="document-body"
+            style={{ textAlign: "center", color: "#52525240" }}
+          >
+            Added content will show here
+          </div>
+        )}
       </div>
     </div>
   );
